c3fda9eb132b1778652c9f4f84ad85dd6005d510
TraceOfHumanity <azalexxx02@gmail.com>
2023-08-05 01:21:29 +0300

    first commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button, Header } from "ui";
+
+export default function Page() {
+  return (
+    <div className="bg-red-700">
+      <Header text="Web" />
+      <Button />
+    </div>
+  );
+}
