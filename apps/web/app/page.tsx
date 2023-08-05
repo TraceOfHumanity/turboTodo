4e0453ed3695e24b1ae86c0bd627273e1f73214f
Turbobot <turbobot@vercel.com>
2023-08-05 15:10:24 +0300

    feat(create-turbo): apply official-starter transform

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,9 @@ import { Button, Header } from "ui";
 
 export default function Page() {
   return (
-    <div className="bg-red-700">
+    <>
       <Header text="Web" />
       <Button />
-    </div>
+    </>
   );
 }
